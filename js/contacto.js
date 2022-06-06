c2a428562742093a90ca306813398c5e34528fbb
Santiago Diaz Monvale <santiago.diaz.monvale@gmail.com>
2022-06-06 20:21:36 -0300

    Arreglando problemas de responsiveness en main

--- js/contacto.js
+++
@@ -1,8 +0,0 @@
-addEventListener ("DOMContentLoaded", function()
-{
-    console.log("El DOM se cargo satisfactoriamente");
-    contactobtn.addEventListener("click", enviardatos)
-});
-function enviardatos(){
-    alert(contactomail.value)
-}
